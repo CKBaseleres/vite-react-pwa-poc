20e9512b067ced14e7759f34ac26f39e8742fcb2
Carl Keneth Baseleres <carl.baseleres@essilor.com>
2023-12-14 15:35:44 +0800

    int api

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
       "x-apikey":"PnEoNwImT0uOLjrtPwge7EBglsMg02G4",
       "showMac":"true"
     }
-    fetch('https://essilor-prd.apigee.net/eu_check_delivery_conformity/v1/ecarte/orders/delivery', {
+    fetch('https://k4f6661dm9.execute-api.eu-west-1.amazonaws.com/v1/ecarte/orders/delivery', {
       method: 'POST',
       headers: headers,
       body: JSON.stringify({
